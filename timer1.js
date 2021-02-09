1488c9854d1be2910e698d2c3e400fc11e73970c
Matthew Cook <matthewcook13987@gmail.com>
2021-02-09 21:08:25 +0000

    Used a for loop to iterate over the input and play a sound at the given intervals provided

--- timer1.js
+++ timer1.js
@@ -1,4 +1,12 @@
 let input = process.argv.splice(2)
 
+//process.stdout.write('\x07');  //Timer noise
 
-console.log(input)
+console.log(`input is: ${input}`)
+
+
+for (const char of input) {
+  setTimeout(() => {
+    process.stdout.write('\x07');
+  }, char * 1000);
+}
